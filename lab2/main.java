db1b78d79448eec985f70c994ca82e37f15dcb64
Bartosz Wlazlowski <43954519+Devoakay@users.noreply.github.com>
2018-10-18 13:44:58 +0200

    Update main.java

--- lab2/main.java
+++ lab2/main.java
@@ -18,31 +18,31 @@ public class main
         String surname;
         while(true)
         {
-            // Podanie imienia z klawiatury
+            
             System.out.print("Enter name (type q to stop program): ");
             name = scanner.next();
-            // Wyswietlenie zawartosci pliku i zakonczenie pracy programu, gdy zostanie wpisane: q
+            
             if(name.equals("q"))
             {
-                // Wyswietlenie zawartosci pliku, gdy plik istnieje
+                
                 if(Files.exists(Paths.get(filePath)))
                     showFileContents();
-                // Zakonczenie pracy programu
+                
                 System.exit(0);
             }
-            // Podanie nazwiska z klawiatury
+            
             System.out.print("Enter surname (type q to stop program): ");
             surname = scanner.next();
-            // Zakonczenie pracy programu, gdy zostanie wpisane: q
+            
             if(surname.equals("q"))
             {
-                // Wyswietlenie zawartosci pliku, gdy plik istnieje
+                
                 if(Files.exists(Paths.get(filePath)))
                     showFileContents();
-                // Zakonczenie pracy programu
+                
                 System.exit(0);
             }
-            // Wygenerowanie e-maila i zapisanie go do pliku
+            
             saveDataToFile(new empo(name, surname));
         }
     }
@@ -51,23 +51,23 @@ public class main
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new FileReader(filePath));
-            // Sprawdzenie czy imie i nazwisko jest w pliku
+            
             String textLine;
             while((textLine = bufferedReader.readLine()) != null)
             {
-                // Usuniecie @domena z tekstu
+                
                 textLine = textLine.substring(0, textLine.indexOf("@"));
-                // Sprawdzenie czy podana osoba jest juz w pliku
+                
                 if(textLine.contains(empo.surname.trim().toLowerCase() + "." + empo.name.trim().toLowerCase()))
                     mailCounter++;
             }
-            // Zwrocenie true, gdy osoba zostala znaleziona w pliku
+            
             if(mailCounter > 0)
                 return true;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            // Zamkniecie pliku
+            
             if(bufferedReader != null) {
                 try {
                     bufferedReader.close();
@@ -80,7 +80,7 @@ public class main
     }
     private static void saveDataToFile(empo empo)
     {
-        // Zapisanie nowego e-maila do pliku
+        
         BufferedWriter bufferedWriter = null;
         String mail;
         try {
@@ -88,23 +88,23 @@ public class main
 
             if(checkIfPersonIsInFile(empo))
             {
-                // Wygenerowanie e-maila z numerem
+                
                 mail = empo.surname.trim().toLowerCase() + "." + empo.name.trim().toLowerCase() + mailCounter + "@mex.com";
-                // Wyzerowanie zmiennej pomocniczej
+                
                 mailCounter = 0;
             }
             else
             {
-                // Wygenerowanie e-maila bez numeru
+                
                 mail = empo.surname.trim().toLowerCase() + "." + empo.name.trim().toLowerCase() + "@mex.com";
             }
-            // Zapisanie e-maila do pliku
+            
             bufferedWriter.write(mail);
             bufferedWriter.newLine();
         } catch(IOException e) {
             e.printStackTrace();
         } finally {
-            // Zamkniecie pliku
+            
             if(bufferedWriter != null) {
                 try {
                     bufferedWriter.close();
@@ -119,18 +119,18 @@ public class main
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new FileReader(filePath));
-            // Wyswietlenie zawartosci pliku
+            
             System.out.println("Saved emails: ");
             String textLine;
             while((textLine = bufferedReader.readLine()) != null)
             {
-                // Wypisanie linii tekstu
+                
                 System.out.println(textLine);
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            // Zamkniecie pliku
+            
             if(bufferedReader != null) {
                 try {
                     bufferedReader.close();
